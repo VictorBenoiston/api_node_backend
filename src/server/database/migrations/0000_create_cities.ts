6cde4fe0f4862771b146e480e4d87326726ba776
Victor Oliveira <63753669+VictorBenoiston@users.noreply.github.com>
2024-04-05 20:09:32 -0300

    feat:created sqlite3 server, migrations and setup knex

---
+++ src/server/database/migrations/0000_create_cities.ts
@@ -0,0 +1,29 @@
+import type { Knex } from 'knex';
+import { EnumTableNames } from '../EnumTableNames';
+
+
+export async function up(knex: Knex) {
+    return knex
+        .schema
+        .createTable(EnumTableNames.cities, table => {
+            table.bigIncrements('id').primary().index();
+            table.string('name', 150).index().notNullable();
+
+            table.comment('Table used to store cities in our system');
+
+        })
+        .then(() => {
+            console.log(`# Create table ${EnumTableNames.cities}`);
+        });
+}
+
+export async function down(knex: Knex) {
+    return knex
+        .schema
+        .dropTable(EnumTableNames.cities)
+
+        .then(() => {
+            console.log(`Rollback! # Droped table ${EnumTableNames.cities}`);
+        });
+}
+
